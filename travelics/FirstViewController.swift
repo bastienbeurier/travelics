c929b16d7a66ea41d15222b5d22e7f85c068027e
Bastien Beurier <bastienbeurier@gmail.com>
2018-10-08 14:19:04 +0200

    Initial Commit

---
+++ travelics/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  travelics
+//
+//  Created by Bastien Beurier on 10/8/18.
+//  Copyright © 2018 Bastien Beurier. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ travelics/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  travelics
+//
+//  Created by Bastien Beurier on 10/8/18.
+//  Copyright © 2018 Bastien Beurier. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
